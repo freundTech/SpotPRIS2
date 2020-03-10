be0ca431062b0cb0e250902747ed1a520382c013
Adrian Freund <adrian@freund.io>
2020-03-10 23:48:11 +0100

    Bump version to 0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(name="SpotPRIS2",
-      version='0.3.1',
+      version='0.4',
       author="Adrian Freund",
       author_email="adrian@freund.io",
       url="https://github.com/freundTech/SpotPRIS2",
